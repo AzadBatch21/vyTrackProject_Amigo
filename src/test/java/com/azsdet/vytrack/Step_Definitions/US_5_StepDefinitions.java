743950feaeac6d3db7655c3c27167aaf624c2d38
Ariz <74077376+Ariz023@users.noreply.github.com>
2021-03-03 20:35:58 -0600

    Added features and snippets

--- src/test/java/com/azsdet/vytrack/Step_Definitions/US_5_StepDefinitions.java
+++ src/test/java/com/azsdet/vytrack/Step_Definitions/US_5_StepDefinitions.java
@@ -1,4 +1,44 @@
 package com.azsdet.vytrack.Step_Definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class US_5_StepDefinitions {
+
+    @Given("truck driver should see all cars cost information on the Vehicle Costs page.")
+    public void truck_driver_should_see_all_cars_cost_information_on_the_vehicle_costs_page() {
+
+    }
+
+
+    @When("truck driver can create Vehicle cost,")
+    public void truck_driver_can_create_vehicle_cost() {
+
+    }
+    @When("truck driver can cancel Vehicle cost,")
+    public void truck_driver_can_cancel_vehicle_cost() {
+
+    }
+    @When("truck driver can edit Vehicle cost,")
+    public void truck_driver_can_edit_vehicle_cost() {
+
+    }
+    @When("truck driver can delete Vehicle cost.")
+    public void truck_driver_can_delete_vehicle_cost() {
+
+    }
+    @When("truck driver can add Attachment on General information page by click on any of the car on the grid.")
+    public void truck_driver_can_add_attachment_on_general_information_page_by_click_on_any_of_the_car_on_the_grid() {
+
+    }
+    @When("truck driver can reset the grid by click on Grid setting")
+    public void truck_driver_can_reset_the_grid_by_click_on_grid_setting() {
+
+    }
+    @Then("truck driver see all cars cost information on the Vehicle Costs page.")
+    public void truck_driver_see_all_cars_cost_information_on_the_vehicle_costs_page() {
+
+    }
+
 }
---
+++ src/test/resources/features/US_5.feature
@@ -0,0 +1,12 @@
+Feature:
+
+Scenario:
+
+  Given truck driver should see all cars cost information on the Vehicle Costs page.
+  When  truck driver can create Vehicle cost,
+  And   truck driver can cancel Vehicle cost,
+  And   truck driver can edit Vehicle cost,
+  And   truck driver can delete Vehicle cost.
+  And   truck driver can add Attachment on General information page by click on any of the car on the grid.
+  And   truck driver can reset the grid by click on Grid setting
+  Then  truck driver see all cars cost information on the Vehicle Costs page.
