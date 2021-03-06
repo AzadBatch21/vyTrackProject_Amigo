8e964840d17cfe91b3bf73d8231e28c87952dfbf
Aliyaguliyeva <aliya.1988@gmail.com>
2021-03-05 22:57:21 -0500

    new commit

--- src/test/java/com/azsdet/vytrack/Pages/LoginPage.java
+++ src/test/java/com/azsdet/vytrack/Pages/LoginPage.java
@@ -3,5 +3,6 @@ package com.azsdet.vytrack.Pages;
 public class LoginPage {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println();
     }
 }
